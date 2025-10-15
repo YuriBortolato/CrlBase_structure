f6b91d61f000300091104cdf92b3aea62c1095a4
YuriBortolato <yurigbort@gmail.com>
2025-10-15 17:40:56 -0300

    feat(test): cria usuário admin padrão na inicialização do sistema

---
+++ BackEnd/api/src/main/java/com/apirest/api/Config/DataLoader.java
@@ -0,0 +1,32 @@
+package com.apirest.api.Config;
+
+import com.apirest.api.entity.Funcionario;
+import com.apirest.api.entity.Cargo;
+import com.apirest.api.repository.FuncionarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner  {
+    private final FuncionarioRepository funcionarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) {
+        // verifica se já existe algum Administrador
+        if (funcionarioRepository.count() == 0) {
+            Funcionario admin = Funcionario.builder()
+                    .nomeCompleto("Administrador do Sistema")
+                    .login("admin")
+                    .senhaCriptografada(passwordEncoder.encode("admin123")) // senha padrão
+                    .cargo(Cargo.ADMIN)
+                    .build();
+
+            funcionarioRepository.save(admin);
+            System.out.println("✅ Usuário administrador criado: login=admin | senha=admin123");
+        }
+    }
+}
