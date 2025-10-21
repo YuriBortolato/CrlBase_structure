6c1f8c4c8799624a8b3d8ea6a17fad008a58aaf0
YuriBortolato <yurigbort@gmail.com>
2025-10-21 18:14:05 -0300

    refactor(api): remove prefixo /api dos controllers

--- BackEnd/api/src/main/java/com/apirest/api/controller/ClienteController.java
+++ BackEnd/api/src/main/java/com/apirest/api/controller/ClienteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clientes")
+@RequestMapping("/clientes")
 @RequiredArgsConstructor
 public class ClienteController {
 
