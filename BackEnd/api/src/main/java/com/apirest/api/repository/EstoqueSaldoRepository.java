6edd1d8d4f4c388c0ce5e606e9701f3abfbfd9d6
YuriBortolato <yurigbort@gmail.com>
2026-02-01 06:29:34 -0300

    Fix: Correção na Query do EstoqueSaldoRepository (Mapeamento idUnidade)

--- BackEnd/api/src/main/java/com/apirest/api/repository/EstoqueSaldoRepository.java
+++ BackEnd/api/src/main/java/com/apirest/api/repository/EstoqueSaldoRepository.java
@@ -2,10 +2,14 @@ package com.apirest.api.repository;
 
 import com.apirest.api.entity.EstoqueSaldo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
 public interface EstoqueSaldoRepository extends JpaRepository<EstoqueSaldo, Long> {
-    Optional<EstoqueSaldo> findByUnidadeIdAndProdutoVariacaoId(Long unidadeId, Long variacaoId);
+
+    @Query("SELECT e FROM EstoqueSaldo e WHERE e.unidade.idUnidade = :unidadeId AND e.produtoVariacao.id = :variacaoId")
+    Optional<EstoqueSaldo> findByUnidadeIdAndProdutoVariacaoId(@Param("unidadeId") Long unidadeId, @Param("variacaoId") Long variacaoId);
 }
